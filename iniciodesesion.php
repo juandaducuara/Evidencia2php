265d29badb77ff78e169f16159d13f145c9bb950
juandaducuara <79880918+juandaducuara@users.noreply.github.com>
2023-04-27 08:51:00 -0500

    Add files via upload

---
+++ iniciodesesion.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <h2>Inicio de sesión</h2>
+    <form action="" method="get">
+        <div class="input">
+            <input type="text" name="Usuario" pattern="[A-Za-z0-9]{4,10}" title="No debe utlizar caracteres especiales" required placeholder="Número de documento" oninput="this.value = this.value.replace(/[^A-Za-z0-9]/g, '').replace(/(\..*)\./g, '$1');">
+        </div>
+        <div class="input">
+            <input type="password" name="Clave" pattern="^[A-Za-z0-9]{3,18}" required placeholder="Ingrese la contraseña" oninput="this.value = this.value.replace(/[^A-Za-z0-9*-_?$.,]/g, '').replace(/(\..*)\./g, '$1');">
+        </div>
+        <div class="input">
+            <input type="submit" value="Ingresar">
+        </div>
+    </form>
+</body>
+
+</html>
